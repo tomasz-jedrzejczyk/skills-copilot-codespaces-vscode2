c543e02edc309c4e225ad2b063203f6bc5edbf87
Tomasz Jędrzejczyk <71791302+tomasz-jedrzejczyk@users.noreply.github.com>
2025-03-01 21:08:22 +0000

    update

--- comments.js
+++ comments.js
@@ -1,13 +1,13 @@
-//crate web server
-//express is a web server framework
+// Create web server
+// Express is a web server framework
 const express = require('express');
 const app = express();
-//create a server
+// Create a server
 const http = require('http');
 const server = http.createServer(app);
-//create a socket
+// Create a socket
 const io = require('socket.io')(server);
-//create a port
+// Create a port
 const PORT = 4000;
-//import the comments array
-const comments = require('./comments.json')
+// Import the comments array
+const comments = require('./comments.json');
